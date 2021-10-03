3e5ea24199a4fc7aae48ed99ef9b9d26f732eee1
Nanda Arfan Hakim <dev.sonadztux@gmail.com>
2021-10-03 11:04:16 +0700

    Update tgtc.sql

--- backend/database/tgtc.sql
+++ backend/database/tgtc.sql
@@ -1,5 +1,235 @@
-CREATE TABLE IF NOT EXISTS "User" ("ID" int PRIMARY KEY, Name text, tier text);
+--
+-- PostgreSQL database dump
+--
 
-CREATE TABLE IF NOT EXISTS "Coupon" ("ID" int PRIMARY KEY, Name text, Type text, Status text, Amount float, image_url text, usertier text, start_date date, expire_date date);
+-- Dumped from database version 12.8 (Ubuntu 12.8-1.pgdg18.04+1)
+-- Dumped by pg_dump version 12.8 (Ubuntu 12.8-1.pgdg18.04+1)
 
-CREATE TABLE IF NOT EXISTS "DetailCoupon" (UserID int, CouponID int, FOREIGN KEY(UserID) REFERENCES "User"("ID"), FOREIGN KEY(CouponID) REFERENCES "Coupon"("ID"));
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: coupon_type; Type: TYPE; Schema: public; Owner: postgres
+--
+
+CREATE TYPE public.coupon_type AS ENUM (
+    'discount',
+    'cashback'
+);
+
+
+ALTER TYPE public.coupon_type OWNER TO postgres;
+
+--
+-- Name: status; Type: TYPE; Schema: public; Owner: postgres
+--
+
+CREATE TYPE public.status AS ENUM (
+    'active',
+    'inactive'
+);
+
+
+ALTER TYPE public.status OWNER TO postgres;
+
+--
+-- Name: tier; Type: TYPE; Schema: public; Owner: postgres
+--
+
+CREATE TYPE public.tier AS ENUM (
+    'bronze',
+    'silver',
+    'gold'
+);
+
+
+ALTER TYPE public.tier OWNER TO postgres;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: coupons; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.coupons (
+    id integer NOT NULL,
+    name text,
+    coupon_type public.coupon_type,
+    status public.status,
+    amount integer,
+    image_url text,
+    usertier public.tier,
+    start_date date,
+    expire_date date
+);
+
+
+ALTER TABLE public.coupons OWNER TO postgres;
+
+--
+-- Name: coupons_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.coupons_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.coupons_id_seq OWNER TO postgres;
+
+--
+-- Name: coupons_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.coupons_id_seq OWNED BY public.coupons.id;
+
+
+--
+-- Name: detail_coupon; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.detail_coupon (
+    user_id integer,
+    coupon_id integer
+);
+
+
+ALTER TABLE public.detail_coupon OWNER TO postgres;
+
+--
+-- Name: users; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.users (
+    id integer NOT NULL,
+    name text,
+    tier public.tier
+);
+
+
+ALTER TABLE public.users OWNER TO postgres;
+
+--
+-- Name: users_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.users_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.users_id_seq OWNER TO postgres;
+
+--
+-- Name: users_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.users_id_seq OWNED BY public.users.id;
+
+
+--
+-- Name: coupons id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.coupons ALTER COLUMN id SET DEFAULT nextval('public.coupons_id_seq'::regclass);
+
+
+--
+-- Name: users id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_id_seq'::regclass);
+
+
+--
+-- Data for Name: coupons; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.coupons (id, name, coupon_type, status, amount, image_url, usertier, start_date, expire_date) FROM stdin;
+\.
+
+
+--
+-- Data for Name: detail_coupon; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.detail_coupon (user_id, coupon_id) FROM stdin;
+\.
+
+
+--
+-- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.users (id, name, tier) FROM stdin;
+\.
+
+
+--
+-- Name: coupons_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.coupons_id_seq', 1, false);
+
+
+--
+-- Name: users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.users_id_seq', 1, false);
+
+
+--
+-- Name: coupons coupons_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.coupons
+    ADD CONSTRAINT coupons_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: detail_coupon fk_coupon_id; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.detail_coupon
+    ADD CONSTRAINT fk_coupon_id FOREIGN KEY (coupon_id) REFERENCES public.coupons(id);
+
+
+--
+-- Name: detail_coupon fk_user_id; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.detail_coupon
+    ADD CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES public.users(id);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
