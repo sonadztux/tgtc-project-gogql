6dff4ba357c14be0ea7577a54929310b4f66d644
alvincteguh <64359340+alvincteguh@users.noreply.github.com>
2021-10-03 10:46:09 +0700

    Add files via upload

--- backend/database/tgtc.sql
+++ backend/database/tgtc.sql
@@ -1,1 +1,5 @@
-CREATE TABLE DetailCoupon (UserID int, CouponID int, FOREIGN KEY(UserID) REFERENCES "User"("ID"), FOREIGN KEY(CouponID) REFERENCES Coupon(ID));
+CREATE TABLE IF NOT EXISTS "User" ("ID" int PRIMARY KEY, Name text, tier text);
+
+CREATE TABLE IF NOT EXISTS "Coupon" ("ID" int PRIMARY KEY, Name text, Type text, Status text, Amount float, image_url text, usertier text, start_date date, expire_date date);
+
+CREATE TABLE IF NOT EXISTS "DetailCoupon" (UserID int, CouponID int, FOREIGN KEY(UserID) REFERENCES "User"("ID"), FOREIGN KEY(CouponID) REFERENCES "Coupon"("ID"));
